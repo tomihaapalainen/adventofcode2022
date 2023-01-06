dbcbca0d0e6544b81b7e47e516123c386255af1d
Tomi Haapalainen <tomi.haapalainen@protonmail.com>
2023-01-06 10:14:37 +0200

    advent of code 2022 day 20b

--- adv20.go
+++ adv20.go
@@ -99,3 +99,78 @@ func adv20a() {
 
 	fmt.Println(result)
 }
+
+func adv20b() {
+	file, _ := os.Open("input20.txt")
+
+	fileScanner := bufio.NewScanner(file)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	original := []int{}
+	for fileScanner.Scan() {
+		row := strings.TrimSpace(fileScanner.Text())
+		rowInt, _ := strconv.ParseInt(row, 10, 32)
+		original = append(original, int(rowInt)*811589153)
+	}
+
+	n := len(original)
+
+	var head *ListNode
+	var cur *ListNode
+	var prev *ListNode
+
+	for i, value := range original {
+		if head == nil {
+			head = &ListNode{i: i, val: value}
+			cur = head
+		} else {
+			cur.next = &ListNode{i: i, val: value}
+			prev = cur
+			cur = cur.next
+			cur.prev = prev
+		}
+	}
+
+	cur.next = head
+	head.prev = cur
+
+	for x := 0; x < 10; x++ {
+		for i, o := range original {
+			cur := head
+
+			for cur.i != i {
+				cur = cur.next
+			}
+
+			if o < 0 {
+				for l := o % (n - 1); l < 0; l++ {
+					cur.val, cur.prev.val = cur.prev.val, cur.val
+					cur.i, cur.prev.i = cur.prev.i, cur.i
+					cur = cur.prev
+				}
+			} else {
+				for l := o % (n - 1); l > 0; l-- {
+					cur.val, cur.next.val = cur.next.val, cur.val
+					cur.i, cur.next.i = cur.next.i, cur.i
+					cur = cur.next
+				}
+			}
+		}
+	}
+
+	cur = head
+	for cur.val != 0 {
+		cur = cur.next
+	}
+
+	result := 0
+	for i := 0; i <= 3000; i++ {
+		if i%1000 == 0 {
+			result += cur.val
+		}
+		cur = cur.next
+	}
+
+	fmt.Println(result)
+}
--- main.go
+++ main.go
@@ -2,4 +2,5 @@ package main
 
 func main() {
 	adv20a()
+	adv20b()
 }
